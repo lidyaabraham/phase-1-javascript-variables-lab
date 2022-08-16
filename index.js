a6f2355045167b9cc44ea805b78f1a6aa6fada79
lidya <lidya. abraham@student.moringaschool.com>
2022-08-16 10:17:43 +0300

    comment goes in here

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+    function companyName = "Scuber"
+    let companyName= "Scuber"
+    // const companyName= "Scuber"
+
+    function mostProfitableNeighborhood = "Chelsea"
+    let mostProfitableNeighborhood = "Chelsea" 
+    // const mostProfitableNeighborhood ="Chelsea"
+
+    function companyCeo = "Susan Smith"
+    let companyCeo = "Susan Smith" 
+    // const companyCeo ="Susan Smith"
